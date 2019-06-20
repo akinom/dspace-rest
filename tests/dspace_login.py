89e366da99da910e44e47fa5d7f8e2f11f84c16e
Monika Mevenkamp <monikam@princeton.edu>
2019-06-20 17:12:20 -0400

    split off login tests

---
+++ tests/dspace_login.py
@@ -0,0 +1,28 @@
+import unittest
+import  dspace, dspace.rest
+import xml.etree.ElementTree as ET
+
+URL = 'https://demo.dspace.org'
+REST = '/rest'
+ADMIN_EMAIL  = 'dspacedemo+admin@gmail.com'
+PWD = 'dspace'
+
+class TestDSpaceRest(unittest.TestCase):
+    def setUp(self):
+        self.api = dspace.rest.Api(URL, REST)
+
+    def test_login(self):
+        user = self.api.login(ADMIN_EMAIL, PWD)
+        self.assertTrue(self.api.authenticated())
+        self.assertTrue(user == self.api.user())
+        self.assertTrue(user == ADMIN_EMAIL)
+
+    def test_login_failure(self):
+        user = self.api.login(ADMIN_EMAIL, PWD + "no-its-not")
+        self.assertFalse(self.api.authenticated())
+        self.assertFalse(self.api.user())
+        self.assertTrue(user == self.api.user())
+
+if __name__ == '__main__':
+    unittest.main()
+
--- tests/dspace_rest.py
+++ tests/dspace_rest.py
@@ -2,14 +2,8 @@ import unittest
 import  dspace, dspace.rest
 import xml.etree.ElementTree as ET
 
-URL = 'https://demo.dspace.org'
 URL = 'https://dataspace.princeton.edu'
-
 REST = '/rest'
-#ADMIN_EMAIL  = 'dspacedemo+admin@gmail.com'
-ADMIN_EMAIL = ''
-PWD = 'dspace'
-#SAMPLE_COMMUNITY_NAME = 'Sample Community'
 SAMPLE_COMMUNITY_NAME = 'Princeton Plasma Physics Laboratory'
 
 class TestDSpaceRest(unittest.TestCase):
@@ -20,25 +14,7 @@ class TestDSpaceRest(unittest.TestCase):
         r = self.api.get("/")
         self.assertTrue(r.status_code == 200)
 
-    def test_login(self):
-        if (ADMIN_EMAIL):
-            user = self.api.login(ADMIN_EMAIL, PWD)
-            self.assertTrue(self.api.authenticated())
-            self.assertTrue(user == self.api.user())
-            self.assertTrue(user == ADMIN_EMAIL)
-
-    def test_login_failure(self):
-        user = self.api.login(ADMIN_EMAIL, PWD + "no-its-not")
-        self.assertFalse(self.api.authenticated())
-        self.assertFalse(self.api.user())
-        self.assertTrue(user == self.api.user())
-
-    def test_login_failure(self):
-        user = self.api.login(ADMIN_EMAIL, PWD + "no-its-not")
-        self.assertFalse(self.api.authenticated())
-        self.assertFalse(self.api.user())
-
-    def test_top_community(self):
+    def test_top_communities(self):
         tops = self.api.topCommunities()
         found_community_with_SAMPLE_NAME = False
         for c in tops:
@@ -47,7 +23,7 @@ class TestDSpaceRest(unittest.TestCase):
             found_community_with_SAMPLE_NAME = found_community_with_SAMPLE_NAME or  (name == SAMPLE_COMMUNITY_NAME)
         self.assertTrue(found_community_with_SAMPLE_NAME)
 
-    def test_sub_community(self):
+    def test_sub_communities(self):
         com = self.find_top_community_by_name(SAMPLE_COMMUNITY_NAME)
         self.assertTrue(com, "can't find community community with name %s" % SAMPLE_COMMUNITY_NAME)
         sub_com = self.api.subCommunities(com)
@@ -56,6 +32,16 @@ class TestDSpaceRest(unittest.TestCase):
             self.assertTrue(s.find('type').text == 'community')
             n = n + 1
         self.assertTrue(n > 0, "expected subcommunities in %s" % (SAMPLE_COMMUNITY_NAME))
+
+    def test_sub_collections(self):
+        com = self.find_top_community_by_name(SAMPLE_COMMUNITY_NAME)
+        self.assertTrue(com, "can't find community community with name %s" % SAMPLE_COMMUNITY_NAME)
+        sub_coll = self.api.collections(com)
+        n = 0
+        for c in sub_coll:
+            self.assertTrue(c.find('type').text == 'collection')
+            n = n + 1
+        self.assertTrue(n > 0, "expected collections in %s" % (SAMPLE_COMMUNITY_NAME))
 
     def find_top_community_by_name(self, com_name):
         tops = self.api.topCommunities()
